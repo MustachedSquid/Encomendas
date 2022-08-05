5fd28fdd9a11070c612bb9c0e98b567eeaf149ff
MustachedSquid <contact@mustachedsquid.com>
2022-08-05 14:22:51 +0100

    Update

---
+++ php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// PATH CONSTANTS
+
+CONST PATH = "/server/root/Encomenda/";
+
+//DB CONSTANTS
+
+CONST SERVERNAME = 'localhost';
+
+CONST USERNAME = '';
+
+CONST PASSWORD = '';
+
+CONST DBNAME = 'Encomendas';
